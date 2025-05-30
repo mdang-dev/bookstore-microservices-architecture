660bc6eb993ca20f154f12aadf6621c791787f6c
mdang-dev <minhdang25.dev@gmail.com>
2025-05-30 22:02:39 +0700

    chore: initialize project structure and configuration

---
+++ catalog-service/src/test/java/com/mddev/bookstore/catalog/TestCatalogServiceApplication.java
@@ -0,0 +1,10 @@
+package com.mddev.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
