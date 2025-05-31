285a43d7ab920a7e2ca209fcdcee8789519a3a2f
mdang-dev <minhdang25.dev@gmail.com>
2025-05-31 22:40:08 +0700

    feat(catalog-service): initialize module setup

--- catalog-service/src/test/java/com/mddev/bookstore/catalog/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/com/mddev/bookstore/catalog/TestCatalogServiceApplication.java
@@ -3,8 +3,9 @@ package com.mddev.bookstore.catalog;
 import org.springframework.boot.SpringApplication;
 
 public class TestCatalogServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.from(CatalogServiceApplication::main)
+        .with(TestcontainersConfiguration.class)
+        .run(args);
+  }
 }
