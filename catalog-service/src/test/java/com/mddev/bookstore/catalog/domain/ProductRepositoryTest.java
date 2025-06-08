bc955a1176980a84c692da8a48761824dee8ee8f
mdang-dev <minhdang25.dev@gmail.com>
2025-06-08 22:13:53 +0700

    feat(catalog-service): setup docker

--- catalog-service/src/test/java/com/mddev/bookstore/catalog/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/mddev/bookstore/catalog/domain/ProductRepositoryTest.java
@@ -2,6 +2,7 @@ package com.mddev.bookstore.catalog.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,22 @@ public class ProductRepositoryTest {
   void shouldGetAllProducts() {
     List<ProductEntity> products = productRepository.findAll();
     assertThat(products).hasSize(15);
+  }
+
+  @Test
+  void shouldGetProductByCode() {
+    ProductEntity product = productRepository.findByCode("B016").orElseThrow();
+    assertThat(product.getCode()).isEqualTo("B016");
+    assertThat(product.getName()).isEqualTo("The Alchemist");
+    assertThat(product.getDescription())
+        .isEqualTo("A novel by Paulo Coelho about a boy pursuing his dream.");
+    assertThat(product.getImageUrl())
+        .isEqualTo("https://covers.openlibrary.org/b/isbn/9780061122415-L.jpg");
+    assertThat(product.getPrice()).isEqualTo(new BigDecimal("10.50"));
+  }
+
+  @Test
+  void shouldReturnProductWhenProductCodeNotExists() {
+    assertThat(productRepository.findByCode("7768")).isEmpty();
   }
 }
